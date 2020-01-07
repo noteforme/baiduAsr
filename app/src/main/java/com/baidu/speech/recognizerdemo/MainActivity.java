fa7fce678ad9c633c62b58edb28247b88ff67641
john <huaiyiheyuan@gmail.com>
2020-01-07 16:47:28 +0800

    first commit

---
+++ app/src/main/java/com/baidu/speech/recognizerdemo/MainActivity.java
@@ -0,0 +1,10 @@
+package com.baidu.speech.recognizerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniRecog;
+
+public class MainActivity extends ActivityMiniRecog {
+}
